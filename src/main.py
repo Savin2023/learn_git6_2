f141ca94fb8230847379fc1fdceb9ecdc84d731b
Savin23 <serg.alex.savin@yandex.ru>
2023-11-18 16:58:58 +0300

    Edit arrange 2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import modules.sko_calc as calc
 import numpy.random as random
 
-a=random.random_integers(1,100,20)
+a=random.random_integers(1,1000,30)
 print("Массив:",a)
 print("\nСтандартное отклонение:", calc.sko(a))
